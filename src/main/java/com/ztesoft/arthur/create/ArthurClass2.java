9cfa088e946d4309058834788648a8da74761a20
Benjamin-GitHub <648822980@qq.com>
2018-03-26 16:14:09 +0800

    changedssssssssss

--- src/main/java/com/ztesoft/arthur/create/ArthurClass2.java
+++ src/main/java/com/ztesoft/arthur/create/ArthurClass2.java
@@ -1,4 +1,8 @@
 package com.ztesoft.arthur.create;
 
 public class ArthurClass2 {
+
+    public static void main(String[] org){
+        System.out.print("arthur changed!");
+    }//changedssssssss
 }
